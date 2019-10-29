4581e00b197c6d2ab9e284f021329245efae6087
Leo <leo881003@gmail.com>
2019-10-29 10:35:19 +0800

    Complete Assignment 10

---
+++ IntroductionToAlgorithm/997.cpp
@@ -0,0 +1,45 @@
+#include <algorithm>
+#include <limits.h>
+#include <stdio.h>
+#include <vector>
+using namespace std;
+#define CURR(x, i) (x[i % 2])
+#define LAST(x, i) (x[((i - 1) % 2)])
+
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    vector<int> A(n);
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &A[i]);
+        sum += A[i];
+    }
+
+    vector<vector<bool>> dp(2, vector<bool>(sum + 1));
+    for (int i = 0; i <= n; i++) {
+        CURR(dp, i)[0] = true;
+        for (int j = 1; j <= sum; j++) {
+            if (i == 0) {
+                CURR(dp, i)[j] = false;
+            } else {
+                // Take
+                if (j - A[i - 1] >= 0) {
+                    CURR(dp, i)[j] = CURR(dp, i)[j] || LAST(dp, i)[j - A[i - 1]];
+                }
+                // Not to Take
+                CURR(dp, i)[j] = CURR(dp, i)[j] || LAST(dp, i)[j];
+            }
+        }
+    }
+    int ans = INT_MAX;
+    for (int j = sum / 2; j >= 0; j--) {
+        if (CURR(dp, n)[j]) {
+            ans = abs((sum - j) - j);
+            break;
+        }
+    }
+    printf("%d\n", ans);
+    return 0;
+}
