f9a09ae7b27853653cbc6e093fe36d74a1f8f114
Leo <leo881003@gmail.com>
2020-11-27 13:41:52 +0800

    Add executor into OS HW1

--- 1188-IntroToOS/HW1-Multitasking/0716073_hw1.py
+++ 1188-IntroToOS/HW1-Multitasking/0716073_hw1.py
@@ -5,6 +5,7 @@ import asyncio
 import hashlib
 import itertools
 import multiprocessing
+from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 import sys
 # External library
 import bs4
@@ -23,8 +24,8 @@ class PowChars:
         else:
             raise StopIteration
 
-def task_pow():
-    S = input()[:5]
+def task_pow(S):
+    #S = input()[:5]
     chrset = iter(PowChars())
 
     for p in itertools.product(chrset, repeat=5):
@@ -34,13 +35,14 @@ def task_pow():
         m.update(CS.encode('utf-8'))
         h = m.hexdigest()
         if h[:5] == '00000':
+            print(CS)
             return CS
         
     return None
 
 
-def task_web_title():
-    url = input()
+def task_web_title(url):
+    #url = input()
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0',
     }
@@ -49,6 +51,40 @@ def task_web_title():
     html = bs4.BeautifulSoup(r.text)
     print(html.title.text)
 
+async def task_web_title_async(loop, url):
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:82.0) Gecko/20100101 Firefox/82.0',
+    }
+
+    r = await loop.run_in_executor(None, requests.get, url, timeout = 30, headers=headers)
+    html = bs4.BeautifulSoup(r.text)
+    print(html.title.text)
+
+def run_pool(pool, task_id, task_count):
+    for i in range(task_count):
+        inp = input()
+        if task_id == 1:
+            pool.submit(task_pow, inp[:5])
+        elif task_id == 2:
+            pool.submit(task_web_title, inp)
+        else:
+            sys.exit('Unknown task!')
+    pool.shutdown(wait=True)
+
+def run_coroutine(task_id, task_count):
+    evloop = asyncio.get_event_loop()
+    tasks = []
+
+    for i in range(task_count):
+        inp = input()
+        if task_id == 1:
+            tasks.append(evloop.create_task(task_web_title_async(evloop, inp)))
+        elif task_id == 2:
+            tasks.append(evloop.run_in_executor(task_pow(inp[:5])))
+        else:
+            sys.exit('Unknown task!')
+
+    evloop.run_until_complete(asyncio.wait(tasks))
 
 def main():
     task_id = int(input())
@@ -64,12 +100,14 @@ def main():
 
     task_count = int(input())
 
-    if task_id == 1:
-        pass
-    elif task_id == 2:
-        pass
+    if worker_type == 1:
+        with ThreadPoolExecutor(worker_count) as pool:
+            run_pool(pool, task_id, task_count)
+    elif worker_type == 2:
+        with ProcessPoolExecutor(worker_count) as pool:
+            run_pool(pool, task_id, task_count)
     else:
-        sys.exit('Unknown task!')
+        run_coroutine(task_id, task_count)
 
 
 if __name__ == '__main__':
