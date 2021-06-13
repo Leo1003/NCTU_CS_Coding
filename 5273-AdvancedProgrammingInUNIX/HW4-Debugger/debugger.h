882470f928f2effd728ab65f3518983458bfb6ab
Leo <leo881003@gmail.com>
2021-06-13 18:51:54 +0800

    Initialize UNIX HW4

---
+++ 5273-AdvancedProgrammingInUNIX/HW4-Debugger/debugger.h
@@ -0,0 +1,29 @@
+#ifndef _HW4_DEBUGGER_H
+#define _HW4_DEBUGGER_H
+
+#include <stdint.h>
+
+typedef struct dbg_ctx {
+    unsigned long prev_disasm_addr, prev_dump_addr;
+} dbg_ctx;
+
+void dbg_ctx_init(dbg_ctx *ctx);
+int dbg_cmd_break(dbg_ctx *ctx, unsigned long addr);
+int dbg_cmd_cont(dbg_ctx *ctx);
+int dbg_cmd_delete(dbg_ctx *ctx, unsigned int id);
+int dbg_cmd_disasm(dbg_ctx *ctx, unsigned long addr);
+int dbg_cmd_dump(dbg_ctx *ctx, unsigned long addr);
+int dbg_cmd_exit(dbg_ctx *ctx);
+int dbg_cmd_get(dbg_ctx *ctx, const char *reg);
+int dbg_cmd_getregs(dbg_ctx *ctx);
+int dbg_cmd_help(dbg_ctx *ctx);
+int dbg_cmd_list(dbg_ctx *ctx);
+int dbg_cmd_load(dbg_ctx *ctx, const char *path);
+int dbg_cmd_run(dbg_ctx *ctx, const char *argv[]);
+int dbg_cmd_vmmap(dbg_ctx *ctx);
+int dbg_cmd_set(dbg_ctx *ctx, const char *reg, unsigned long value);
+int dbg_cmd_si(dbg_ctx *ctx);
+int dbg_cmd_start(dbg_ctx *ctx, const char *argv[]);
+
+#endif
+
